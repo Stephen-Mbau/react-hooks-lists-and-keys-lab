977cdc8917fb813815ae2ac60f4c7e63b02e94d7
Stephen Mbau <mbaustephen@gmail.com>
2023-08-25 10:06:11 +0300

    Done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <ul>
+          {technologies.map((technology) => (
+            <span key={technology}>{technology}</span> 
+          ))}
+        </ul>
       </div>
     </div>
   );
